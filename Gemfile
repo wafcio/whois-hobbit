494965b585b11529143f2f9a71377a0896dfd431
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2015-03-07 19:09:31 +0100

    render page setup

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source 'https://rubygems.org'
 
 gem 'hobbit'
+gem 'hobbit-contrib'
 gem 'thin'
+gem 'haml'
 
 gem 'rake'
 gem 'pry'
--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,25 @@
 require_relative 'environment'
 
 require 'hobbit'
+require 'hobbit/render'
+require 'haml'
 
 module Whois
   class Application < Hobbit::Base
+    include Hobbit::Render
+
+    get '/' do
+      render 'home'
+    end
+
+    private
+
+    def template_engine
+      'haml'
+    end
+
+    def views_path
+      'app/views'
+    end
   end
 end
